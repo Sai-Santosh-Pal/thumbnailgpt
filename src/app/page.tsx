2af75f51c82a32b2e62649e350967047510fb608
Sai Santosh Pal <saisantoshpal2010@gmail.com>
2025-06-28 15:33:02 +0530

    fixed import bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { ProductShowcase } from "@/sections/ProductShowcase";
-import { Pricing } from "@/sections/Pricing";
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
       <Hero />
       <LogoTicker />
       <ProductShowcase/>
-      <Pricing />
       </>
     );
 }
